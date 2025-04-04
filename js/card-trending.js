19f314c0e6c6611a9e964a58b1f515b31b65c371
esti-martin <estibalizmartinborja@gmail.com>
2025-04-04 11:26:23 +0200

    card trending responsive (falta la caja de #)

--- js/card-trending.js
+++ js/card-trending.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.card-trending');
+    const buttons = document.querySelectorAll('.btn-princ');
 
+    // Alterna la clase 'active' en la tarjeta seleccionada
     cards.forEach(card => {
-        card.addEventListener('click', () => {
-            // Alterna la clase 'active' en la tarjeta seleccionada
+        card.addEventListener('click', () => {           
             card.classList.toggle('active');
+        });
+    });
+
+    // Evita que el clic en el botón cierre la tarjeta
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.stopPropagation(); // Detiene la propagación del evento al contenedor padre
+            console.log('Botón clickeado');
         });
     });
 });
